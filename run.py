1e042c5e6bfa57c883450f7b37d06e76874bd064
Oliver Griffin <olivergriffin2991@gmail.com>
2024-08-15 19:55:51 +0000

    started welcome message

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
+# imported textwrap to improve visuals
+import textwrap
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -14,5 +16,17 @@ SHEET = GSPREAD_CLIENT.open("weekly_creche_food_budget")
 
 budget = SHEET.worksheet("budget")
 
-data = budget.get_all_values()
-print(data)
+def welcome_message():
+    """
+    Displays the welcome message
+    """
+    print("Welcome to your creche food budget calculator!\n")
+    print(textwrap.fill("Use this handy calculator to determine how" 
+                        " much your weekly food spend on vegetables and meat should be."))
+    print()
+    print(textwrap.fill("The calculator will take inputs of the predicted attendance of" 
+                        " children and staff for the coming week and ask if there are" 
+                        " any vegetarians present. Then it will output your individual" 
+                        " budgets for Meat and Vegetables to a spreadsheet."))
+                        
+welcome_message()
