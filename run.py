60a4bd262848814e392993a5cc77e80f2a342e58
Oliver Griffin <olivergriffin2991@gmail.com>
2024-08-16 16:52:28 +0000

    created get_week_starting func and validate_date func

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ import gspread
 from google.oauth2.service_account import Credentials
 # imported textwrap to improve visuals
 import textwrap
+# imported datetime to help with date storage
+import datetime
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -28,5 +30,50 @@ def welcome_message():
                         " children and staff for the coming week and ask if there are" 
                         " any vegetarians present. Then it will output your individual" 
                         " budgets for Meat and Vegetables to a spreadsheet."))
-                        
-welcome_message()
+
+def get_week_starting():
+    """
+    Gets the current date from the user
+    """
+    print("Let's get you started!\n")
+    while True:
+        print("Please enter the starting date for the week")
+        print("Enter the year: ")
+        year = input()
+
+        print("Enter the month: ")
+        month = input()
+
+        print("Enter the day: ")
+        day = input()
+
+        validate_date(year, month, day)
+
+        if validate_date(year, month, day):
+            print("Date is valid!")
+            break
+    
+    
+    
+
+def validate_date(y, m, d):
+
+    try:
+        week_starting = datetime.date(int(y), int(m), int(d))
+    
+    except ValueError as e:
+        print("Date is invalid please enter as YYYY MM DD")
+        return False
+
+    return True
+
+
+
+def main():  
+    """
+    Run all program functions
+    """             
+    welcome_message()
+    get_week_starting()
+
+main() 
